99b4bacf30e0d36d87d36cf368830dc4698cf0ad
Denis Kushner <ateo.immortal228@gmail.com>
2024-06-08 16:25:22 +0300

    Add AddProfilePhotos

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HomePage, CreateProfilePage } from "./pages";
 
 import "./App.css";
+import { AddProfilePhotos } from "./pages/AddProfilePhotos";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create-profile" element={<CreateProfilePage />} />
+        <Route path="/add-profile-photos" element={<AddProfilePhotos />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/AddProfilePhotos.tsx
@@ -0,0 +1,5 @@
+export const AddProfilePhotos = () => {
+    return (<>
+        <h1>AddProfilePhotos</h1>
+    </>)
+}
