0114c134ac8e85664ed22e7857dff4bc9d4dde80
Denis Kushner <ateo.immortal228@gmail.com>
2024-06-09 00:55:09 +0300

    Added style in CreateProfilePage

--- src/pages/AddProfilePhotoPage/AddProfilePhotoPage.tsx
+++ src/pages/AddProfilePhotoPage/AddProfilePhotoPage.tsx
@@ -5,6 +5,7 @@ const CreateProfilePage = () => {
   const [photo, setPhoto] = useState<File>();
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.files?.[0])
     const file = event.target.files?.[0];
 
     if (file) {
@@ -19,17 +20,19 @@ const CreateProfilePage = () => {
   return (
     <form className="profilePage__form">
       <h1 className="profilePage__form-title">Add profile photos</h1>
-      {!!photo && renderPhoto(photo)}
-      <input
-        type="file"
-        name="photo"
-        className="profilePage__form-inputPhoto"
-        onChange={handleFileChange}
-        multiple
-      />
+      <div className="profilePage__form-photo">
+        {!!photo && renderPhoto(photo)}
+        <input
+          type="file"
+          name="photo"
+          className="profilePage__form-inputPhoto"
+          onChange={handleFileChange}
+          multiple
+        />
+      </div>
 
-      <p>You must add 3 photos to continue</p>
-      <button type="submit">Continue</button>
+      <p className="profilePage__form-text">You must add 3 photos to continue</p>
+      <button className="btn" type="submit"><span>Continue</span></button>
     </form>
   );
 };
