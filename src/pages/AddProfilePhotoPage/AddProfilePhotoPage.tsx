4602dc49416926f964b6f8fcbcb2d41744f53edc
Ilya Kohanovskiy <i.kohanvs@gmail.com>
2024-06-09 00:16:05 +0300

    file input fix

--- src/pages/AddProfilePhotoPage/AddProfilePhotoPage.tsx
+++ src/pages/AddProfilePhotoPage/AddProfilePhotoPage.tsx
@@ -1,28 +1,37 @@
-import { useState } from "react";
-import './AddProfilePhotoPage.css'
+import { ChangeEvent, useState } from "react";
+import "./AddProfilePhotoPage.css";
 
 const CreateProfilePage = () => {
-  const [photo, setPhoto] = useState()
+  const [photo, setPhoto] = useState<File>();
 
-  const renderPhoto = (photo:string | undefined) => {
-    {if (photo !== undefined) {
-      return <img src={photo} alt="photo" />  
-    } else {
-      return
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file) {
+      setPhoto(file);
     }
-  }
-  }
+  };
+
+  const renderPhoto = (photo: File) => {
+    return <img src={URL.createObjectURL(photo)} alt="photo" />;
+  };
 
   return (
-        <form className="profilePage__form">
-          <h1 className="profilePage__form-title">Add profile photos</h1>
-          {renderPhoto(photo)}
-          <input type="file" name="photo" className="profilePage__form-inputPhoto" onChange={(e:string)=>setPhoto(e.target.value)} multiple/>
+    <form className="profilePage__form">
+      <h1 className="profilePage__form-title">Add profile photos</h1>
+      {!!photo && renderPhoto(photo)}
+      <input
+        type="file"
+        name="photo"
+        className="profilePage__form-inputPhoto"
+        onChange={handleFileChange}
+        multiple
+      />
 
-          <p>You must add 3 photos to continue</p>
-          <button type="submit">Continue</button>
-        </form>
-  )
+      <p>You must add 3 photos to continue</p>
+      <button type="submit">Continue</button>
+    </form>
+  );
 };
 
 export default CreateProfilePage;
