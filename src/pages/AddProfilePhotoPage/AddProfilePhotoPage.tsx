f03a6ca7f6954e692cf259b0cebfaceb70a5331c
AteoImmortal <105009513+AteoImmortal@users.noreply.github.com>
2024-06-09 12:25:18 +0300

    Add files via upload

--- src/pages/AddProfilePhotoPage/AddProfilePhotoPage.tsx
+++ src/pages/AddProfilePhotoPage/AddProfilePhotoPage.tsx
@@ -1,10 +1,28 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useContext, useEffect, useState } from "react";
 import "./AddProfilePhotoPage.css";
 import { Button } from "../../components";
 import { useNavigate } from "react-router-dom";
+import { showBackButton } from "../../lib/telegram";
+import { useForm } from "react-hook-form";
+import { UserProfileContext } from "../../context/UserProfileContext";
 
 const CreateProfilePage = () => {
   const [photo, setPhoto] = useState<File>();
+  const { userProfile, setUserProfile } = useContext(UserProfileContext);
+
+  const { register, handleSubmit } = useForm<IFormInput>({});
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    showBackButton();
+  }, []);
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    setUserProfile({ ...userProfile, ...data });
+
+    navigate("/suggestion");
+  };
+
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.files?.[0])
@@ -19,11 +37,11 @@ const CreateProfilePage = () => {
     return <img src={URL.createObjectURL(photo)} alt="photo" />;
   };
 
-  const navigate = useNavigate();
 
   return (
     <form 
-      className="profilePage__form">
+      className="profilePage__form"
+      onSubmit={handleSubmit(onSubmit)}>
       <h1 className="profilePage__form-title">Add profile photos</h1>
       <div className="profilePage__form-photo">
         {!!photo && renderPhoto(photo)}
@@ -37,7 +55,7 @@ const CreateProfilePage = () => {
       </div>
 
       <p className="profilePage__form-text">You must add 3 photos to continue</p>
-      <Button onSubmit={navigate("/suggestion")}  type="submit">Continue</Button>
+      <Button type="submit">Continue</Button>
     </form>
   );
 };
--- src/pages/AddProfilePhotoPage/index.ts
+++ src/pages/AddProfilePhotoPage/index.ts
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
 export { default as CreateProfilePage } from "./AddProfilePhotoPage";
 =======
+<<<<<<< HEAD
 export { default as CreateProfilePage } from "./AddProfilePhotoPage";
+=======
+export { default as CreateProfilePage } from "./AddProfilePhotoPage";
+>>>>>>> b5d3280c9bcfb91335e1ac7bc955e9455fbe1792
 >>>>>>> 71780c262d38a5153ccef48c9a04db4ea592b02a
