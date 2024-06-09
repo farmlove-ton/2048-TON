71780c262d38a5153ccef48c9a04db4ea592b02a
AteoImmortal <105009513+AteoImmortal@users.noreply.github.com>
2024-06-09 12:05:37 +0300

    Added CreateProfilePhotoPage

---
+++ src/pages/AddProfilePhotoPage/AddProfilePhotoPage.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent, useState } from "react";
+import "./AddProfilePhotoPage.css";
+import { Button } from "../../components";
+import { useNavigate } from "react-router-dom";
+
+const CreateProfilePage = () => {
+  const [photo, setPhoto] = useState<File>();
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.files?.[0])
+    const file = event.target.files?.[0];
+
+    if (file) {
+      setPhoto(file);
+    }
+  };
+
+  const renderPhoto = (photo: File) => {
+    return <img src={URL.createObjectURL(photo)} alt="photo" />;
+  };
+
+  const navigate = useNavigate();
+
+  return (
+    <form 
+      className="profilePage__form"
+      onSubmit={navigate("/suggestion")}>
+      <h1 className="profilePage__form-title">Add profile photos</h1>
+      <div className="profilePage__form-photo">
+        {!!photo && renderPhoto(photo)}
+        <input
+          type="file"
+          name="photo"
+          className="profilePage__form-inputPhoto"
+          onChange={handleFileChange}
+          multiple
+        />
+      </div>
+
+      <p className="profilePage__form-text">You must add 3 photos to continue</p>
+      <Button  type="submit">Continue</Button>
+    </form>
+  );
+};
+
+export default CreateProfilePage;
